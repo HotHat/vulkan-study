fefb74791b0bc8ea5e732fe4136656bbf7193c33
HotHat <luyuhux@gmail.com>
2025-10-28 21:23:58 +0800

    add device

--- src/physical_device.h
+++ src/physical_device.h
@@ -270,6 +270,95 @@ private:
     PhysicalDevice PopulateDeviceDetails(VkPhysicalDevice phys_device, FeatureChain const &src_extended_features_chain) const;
     PhysicalDevice::Suitable IsDeviceSuitable(PhysicalDevice const &phys_device, std::vector<std::string> &unsuitability_reasons) const;
 };
+
+
+// ---- Queue ---- //
+enum class QueueType { kPresent, kGraphics, kCompute, kTransfer };
+    // ---- Device ---- //
+
+struct Device {
+    VkDevice device = VK_NULL_HANDLE;
+    PhysicalDevice physical_device;
+    VkSurfaceKHR surface = VK_NULL_HANDLE;
+    std::vector<VkQueueFamilyProperties> queue_families;
+    VkAllocationCallbacks* allocation_callbacks = nullptr;
+    PFN_vkGetDeviceProcAddr fp_vkGetDeviceProcAddr = nullptr;
+    uint32_t instance_version = VK_API_VERSION_1_0;
+
+    uint32_t get_queue_index(QueueType type) const;
+    // Only a compute or transfer queue type is valid. All other queue types do not support a 'dedicated' queue index
+    uint32_t GetDedicatedQueueIndex(QueueType type) const;
+
+    VkQueue get_queue(QueueType type) const;
+    // Only a compute or transfer queue type is valid. All other queue types do not support a 'dedicated' queue
+    VkQueue get_dedicated_queue(QueueType type) const;
+
+    // A conversion function which allows this Device to be used
+    // in places where VkDevice would have been used.
+    explicit operator VkDevice() const;
+
+    private:
+    struct {
+        PFN_vkGetDeviceQueue fp_vkGetDeviceQueue = nullptr;
+        PFN_vkDestroyDevice fp_vkDestroyDevice = nullptr;
+    } internal_table;
+    friend class DeviceBuilder;
+    friend void destroy_device(Device const& device);
+};
+
+
+// For advanced device queue setup
+struct CustomQueueDescription {
+    explicit CustomQueueDescription(uint32_t index, std::vector<float> const& priorities)
+    : index(index), priorities(priorities) {}
+
+    explicit CustomQueueDescription(uint32_t index, std::vector<float>&& priorities)
+    : index(index), priorities(std::move(priorities)) {}
+
+    explicit CustomQueueDescription(uint32_t index, size_t count, float const* priorities)
+    : index(index), priorities(priorities, priorities + count) {}
+
+    uint32_t index;
+    std::vector<float> priorities;
+};
+
+void destroy_device(Device const& device);
+
+class DeviceBuilder {
+    public:
+    // Any features and extensions that are requested/required in PhysicalDeviceSelector are automatically enabled.
+    explicit DeviceBuilder(PhysicalDevice physical_device);
+
+    Device build() const;
+
+    // For Advanced Users: specify the exact list of VkDeviceQueueCreateInfo's needed for the application.
+    // If a custom queue setup is provided, getting the queues and queue indexes is up to the application.
+    DeviceBuilder& custom_queue_setup(size_t count, CustomQueueDescription const* queue_descriptions);
+    DeviceBuilder& custom_queue_setup(std::vector<CustomQueueDescription> const& queue_descriptions);
+    DeviceBuilder& custom_queue_setup(std::vector<CustomQueueDescription>&& queue_descriptions);
+#if VKB_SPAN_OVERLOADS
+    DeviceBuilder& custom_queue_setup(std::span<const CustomQueueDescription> queue_descriptions);
+#endif
+
+    // Add a structure to the pNext chain of VkDeviceCreateInfo.
+    // The structure must be valid when DeviceBuilder::build() is called.
+    template <typename T> DeviceBuilder& add_pNext(T* structure) {
+        info.next_chain.push_back(structure);
+        return *this;
+    }
+
+    // Provide custom allocation callbacks.
+    DeviceBuilder& set_allocation_callbacks(VkAllocationCallbacks* callbacks);
+
+    private:
+    PhysicalDevice physical_device;
+    struct DeviceInfo {
+        VkDeviceCreateFlags flags = static_cast<VkDeviceCreateFlags>(0);
+        std::vector<void*> next_chain;
+        std::vector<CustomQueueDescription> queue_descriptions;
+        VkAllocationCallbacks* allocation_callbacks = nullptr;
+    } info;
+};
 } // end namespace lvk
 
 #endif  // LVK_PHYSICAL_DEVICE_H
